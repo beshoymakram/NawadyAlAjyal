98cea325708e3b33d7ede3026b4530f3f2058b47
Beshoy Makram <83455686+beshoymakram@users.noreply.github.com>
2023-02-26 18:25:42 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+$('#owl-1').owlCarousel({
+    loop: true,
+    margin: 30,
+    padding: 0,
+    nav: true,
+    autoHeight: true,
+    dots: false,
+    navText: ['<i class="fa-solid bg-white rounded-circle fa-circle-arrow-left"></i>', '<i class="fa-solid bg-white rounded-circle fa-circle-arrow-right"></i>'],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+$('#owl-2').owlCarousel({
+    stagePadding: 200,
+    loop:true,
+    margin:0,
+    nav:false,
+    items:1,
+    lazyLoad: true,
+  responsive:{
+        0:{
+            items:1,
+            stagePadding: 10
+        },
+        600:{
+            items:1,
+            stagePadding: 100
+        },
+        1000:{
+            items:1,
+            stagePadding: 200
+        },
+        1200:{
+            items:1,
+            stagePadding: 250
+        },
+        1400:{
+            items:1,
+            stagePadding: 300
+        },
+        1600:{
+            items:1,
+            stagePadding: 350
+        },
+        1800:{
+            items:1,
+            stagePadding: 400
+        }
+    }
+})
